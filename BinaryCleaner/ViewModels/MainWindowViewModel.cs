1b2c17b7b220957d329815f7b0d3b6622c70bf93
Vincent Neparidze <negator92@gmail.com>
2020-05-17 20:22:56 +0300

    DockPanel added to main window

---
+++ BinaryCleaner/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryCleaner.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => nameof(BinaryCleaner);
+    }
+}
