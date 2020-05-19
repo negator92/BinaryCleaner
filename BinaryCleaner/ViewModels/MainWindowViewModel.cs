6382e840b9c0e214f0467899ff3a1ea13b30a736
Vincent Neparidze <negator92@gmail.com>
2020-05-19 22:36:15 +0300

    Adding OpenFolderDialog and some style improvements

--- BinaryCleaner/ViewModels/MainWindowViewModel.cs
+++ BinaryCleaner/ViewModels/MainWindowViewModel.cs
@@ -7,5 +7,6 @@ namespace BinaryCleaner.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => nameof(BinaryCleaner);
+        public string PathToClean { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
     }
 }
